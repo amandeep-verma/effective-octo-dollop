dcef55b0ebcb3ec8cf7d88e6a1509d5041947122
amandeep-verma <39786879+amandeep-verma@users.noreply.github.com>
2021-04-03 01:13:37 -0400

    I intake all.

---
+++ TestObject/src/main/java/com/telusko/hell/TestObject/Controller.java
@@ -0,0 +1,11 @@
+package com.telusko.hell.TestObject;
+
+public class Controller {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("coz am the one");
+	}
+
+}
